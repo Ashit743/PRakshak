f8a484bf3b84807426a91291c2d7204dcd6178a1
Amit Kumar Singh <131665134+amitkumarsingh007@users.noreply.github.com>
2024-02-23 12:20:26 +0530

    Revert "router and ui done"

--- P_Rakshak/src/components/SideBar.jsx
+++ P_Rakshak/src/components/SideBar.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import "../App.css"
-
 export default function SideBar() {
   return (
     <div>
@@ -14,41 +12,13 @@ export default function SideBar() {
           </li>
 
           <ul className='mt-3 menu-items-wrapper'>
-
+            <li><a href="#"></a><img src='assets/patients.png' height={30} /></li>
+            <li><a href="#"><img src='assets/calendar.png' height={25} /></a></li>
+            <li><a href="#"><img src='assets/doctor.png' height={25} /></a></li>
             <li>
-              <Link to="">
-                <img src='assets/patients.png' height={30} />
-              </Link>
-            </li>
-
-
-            <li>
-              <Link to="/Calender">
-                <img src='assets/calendar.png' height={25} />
-              </Link>
-            </li>
-
-
-            <li>
-              <Link to="/Doctor">
-                <img src='assets/doctor.png' height={25} />
-              </Link>
-            </li>
-
-
-            <li>
-              <Link to="/Notifications">
-                <img src='assets/notifications.png' height={25} />
-              </Link>
+              <a href="#"><img src='assets/notifications.png' height={25} /></a>
               <span className='notification-count'>5</span>
             </li>
-
-            <li>
-              <Link to="/">
-              <img src='assets/call.png' height={25} />
-              </Link>
-            </li>
-
           </ul>
         </ul>
       </div>
--- P_Rakshak/src/components/VitalSign.jsx
+++ P_Rakshak/src/components/VitalSign.jsx
@@ -4,22 +4,7 @@ import "../App.css"
 export default function VitalSign() {
   return (
     <div>
-
-      <div className='row me-2 ms-2'>
-        <div className='col-12 vital-heading'>
-          <h6 className='m-0'>Vital Sings</h6>
-        </div>
-        <div className='col-12 mt-4 text-center'>
-          <div className='row justify-content-between'>
-            <div className='col-3 vital-card'> test</div>
-            <div className='col-3 vital-card'> test</div>
-            <div className='col-3 vital-card'> test</div>
-            <div className='col-3 vital-card'> test</div>
-          </div>
-        </div>
-      </div>
-
-
+     <div>VitalSign</div>
     </div>
   )
 }
