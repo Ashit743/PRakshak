01ebabf5f58687778475bce67de9719d1c1595dd
Amit Kumar Singh <131665134+amitkumarsingh007@users.noreply.github.com>
2024-02-23 12:33:58 +0000

    ui changes

--- P_Rakshak/src/routes.jsx
+++ P_Rakshak/src/routes.jsx
@@ -4,7 +4,12 @@ import {
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import VerifyOTP from "./pages/VerifyOTP";
-import Prediction from "./components/prediction/prediction"
+import Prediction from "./components/prediction/prediction";
+import Layout from "./components/UI/Layout";
+import MonthlyCalendar from "./components/calendar/Calendar";
+import DoctorCard from "./components/doctor-cards/doctorCards";
+import EmergencyCall from "./components/EmergencyCall";
+import Notifications from "./components/Notifications";
 export const router = createBrowserRouter([
     {
         path: "/signup",
@@ -12,14 +17,32 @@ export const router = createBrowserRouter([
     },
     {
         path: "/",
-        element: <Home />
-    },{
+        element: <Layout><Home /></Layout>
+    }, 
+    {
+        path: "/prediction",
+        element:<Layout><Prediction/></Layout>
+    },
+    {
+        path: "/calendar",
+        element: <Layout>< MonthlyCalendar/></Layout>
+    },
+    {
+        path: "/doctor",
+        element: <Layout>< DoctorCard/></Layout>
+    },
+    {
+        path: "/notifications",
+        element: <Layout>< Notifications/></Layout>
+    },
+    {
+        path: "/emergency",
+        element: <Layout>< EmergencyCall/></Layout>
+    },
+    {
         path:"/verify",
         element:<VerifyOTP/>
     },
-    {
-        path: "/prediction",
-        element:<Prediction/>
-    }
+   
 ])
 
