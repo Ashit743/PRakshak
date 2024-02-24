9780e7fcf2253daaca1978dca9ebdb326c203a01
Ashit <115707005+ashitpersistent@users.noreply.github.com>
2024-02-24 21:50:08 +0000

    flask food recommendation api call

--- P_Rakshak/src/service/index.js
+++ P_Rakshak/src/service/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://fantastic-space-fortnight-76q6gjxqj6gfg9v-5000.app.github.dev",
 });
 export const mlapi = axios.create({
-  baseURL: "https://ominous-capybara-vwrp4xqj6g5hvrv-5050.app.github.dev",
+  baseURL: "https://fantastic-space-fortnight-76q6gjxqj6gfg9v-5050.app.github.dev",
 });
 export default api;
