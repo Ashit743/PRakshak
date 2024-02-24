a81700337e469f364db0382e03af7fb22f25df93
varun_r <varun.r.mrv@gmail.com>
2024-02-24 11:53:06 +0530

    db file changes

--- backend/db.py
+++ backend/db.py
@@ -1,15 +1,15 @@
 import pymysql
-
+import os
 timeout = 10
 connection = pymysql.connect(
   charset="utf8mb4",
   connect_timeout=timeout,
   cursorclass=pymysql.cursors.DictCursor,
   db="myDatabase",
-  host="mysql-573b75b-prakshak-1234.a.aivencloud.com",
-  password="AVNS_xow4bsXavI6bmPG-GaS",
+  host= os.environ["AIVEN_DB_HOST"],
+  password= os.environ["AIVEN_DB_PASS"],
   read_timeout=timeout,
-  port=21392,
+  port=int(os.environ["AIVEN_DB_PORT"]),
   user="avnadmin",
   write_timeout=timeout,
 )
